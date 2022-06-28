b175316dd40c9def18b47d8af5b1c2dbab8a9384
Gourav1221 <90262724+Gourav1221@users.noreply.github.com>
2022-06-28 16:34:48 +0530

    add new db

--- src/configs/db.js
+++ src/configs/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 
 module.exports = ()=>{
-    return mongoose.connect("mongodb://localhost:27017/Taskfor")
+    return mongoose.connect("mongodb+srv://gourab:gourab2001@cluster0.fgw7e.mongodb.net/?retryWrites=true&w=majority")
 }
 
 //"mongodb://localhost:27017/auth-mvc"
