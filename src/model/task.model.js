d6d5f5f28383892a43d9e472d831d55bb3017f1f
Gourav1221 <90262724+Gourav1221@users.noreply.github.com>
2022-06-28 16:13:37 +0530

    changes in model

--- src/model/task.model.js
+++ src/model/task.model.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 
 const TaskSchema =  mongoose.Schema({
 
-    id:{type: Number, required: true},
+    //id:{type: Number, required: true},
     taskName: {type:String, required:true,unique:true},
     userName:{type:String, required:true}
 
